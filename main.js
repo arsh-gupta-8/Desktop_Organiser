6794308249bcf64d874eb77f7462e526aad5d101
arsh-gupta-8 <arshmailing@gmail.com>
2026-08-23 23:45:17 +0700

    Added auto open dev tool and fixed window issue for mac

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron')
 
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Desktop Organiser',
-    width: 800,
+    width: isDev ? 1300 : 800,
     height: 500,
   });
- 
+  
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow()
+    }
+  })
 })
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit()
+  }
+})  
